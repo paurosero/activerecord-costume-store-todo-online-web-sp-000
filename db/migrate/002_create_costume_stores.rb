7a9840acbfdd41fecae56360e113c8dc71d8986b
Paul Rosero <paul_rosero@yahoo.com>
2019-07-18 20:51:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       c.string :name
       c.string :location 
       c.integer :costume_inventory
-      c.integer :number_of_employees 
-      c.boolean
+      c.integer :num_of_employees 
+      c.boolean :
       c.timestamps
     end
   end 
