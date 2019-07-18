6e99b073528b801c3088f0b8104c5b71d11ab8ef
Paul Rosero <paul_rosero@yahoo.com>
2019-07-18 21:00:50 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |c|
+      c.string :name
+      c.string :location 
+      c.string :theme
+      c.integer :price
+      c.boolean :still_in_business
+      c.datetime :opening_time
+      c.datetime :closing_time
+    end
+  end 
+end
